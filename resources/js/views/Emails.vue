508baa07d1392f9faa8da21a966746f88a9064ee
Mohamed Hassan <hassan.mohamed.sf@gmail.com>
2019-08-28 09:07:09 +0200

    Created Vue.js Emails List view

---
+++ resources/js/views/Emails.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>Emails</h1>
+
+    <table class="table table-striped table-hover">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Recipients</th>
+          <th>Subject</th>
+          <th>Type</th>
+          <th>Options</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(email, index) in emails" :key="index">
+          <td>{{email.id}}</td>
+          <td>{{email.recipients_count}}</td>
+          <td>{{email.subject}}</td>
+          <td>{{email.type}}</td>
+          <td>
+            <a href="#">View</a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Emails",
+  data() {
+    return {
+      emails: []
+    };
+  },
+  created() {
+    axios
+      .get("http://127.0.0.1:8000/api/emails")
+      .then(result => {
+        this.emails = result.data.data;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+};
+</script>
