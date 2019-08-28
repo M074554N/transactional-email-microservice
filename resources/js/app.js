a1093606a5313f6b15bbf04b370eb50cf5ee4def
Mohamed Hassan <hassan.mohamed.sf@gmail.com>
2019-08-28 09:09:29 +0200

    Update App.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,37 @@
-require('./bootstrap');
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-window.Vue = require('vue');
+Vue.use(VueRouter)
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+import App from './views/App';
+import Home from './views/Home';
+import Emails from './views/Emails';
+import CreateEmail from './views/CreateEmail';
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [{
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/emails',
+            name: 'emails',
+            component: Emails,
+        },
+        {
+            path: '/create-email',
+            name: 'create-email',
+            component: CreateEmail,
+        },
+    ],
+});
 
 const app = new Vue({
     el: '#app',
+    components: {
+        App
+    },
+    router,
 });
