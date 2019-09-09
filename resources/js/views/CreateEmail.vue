ce7271a829159c16ffaff2b19abac43b3ea99dfc
Mohamed Hassan <hassan.mohamed.sf@gmail.com>
2019-09-09 10:03:39 +0200

    adding new editors and handling API calls

--- resources/js/views/CreateEmail.vue
+++ resources/js/views/CreateEmail.vue
@@ -2,88 +2,170 @@
   <div>
     <h1>Create a New Email</h1>
     <br />
+    <p class="alert alert-danger" v-if="error != null">{{error}}</p>
+    <p class="alert alert-success" v-if="success != null">{{success}}</p>
     <br />
     <div class="row">
       <div class="col">
         <div class="form-group">
-          <label for="subject">Message Subject:</label>
-          <input v-model="subject" type="text" class="form-control" id="subject" />
+          <label for="subject" class="bold">Message Subject:</label>
+          <input
+            v-model="subject"
+            type="text"
+            class="form-control"
+            id="subject"
+            :disabled="sending"
+            placeholder="Email Subject"
+          />
         </div>
       </div>
       <div class="col">
         <div class="form-group">
-          <label for="recipients">Recipients:</label>
+          <label for="recipients" class="bold">Recipients:</label>
           <vue-tags-input
+            id="recipients"
             v-model="tag"
             :validation="validation"
             :allow-edit-tags="true"
             @tags-changed="newTags => recipients = newTags"
             aria-describedby="emailHelp"
+            :separators="[',']"
+            placeholder="Add Email Address"
+            :disabled="sending"
           />
           <small
             id="emailHelp"
             class="form-text text-muted"
-          >Enter comma separated list of valid email addresses of recipients.</small>
+          >Enter or paste a comma separated list of valid email addresses of recipients.</small>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="form-group">
+          <label class="bold">Message Type:</label>
+          <br />
+          <div class="form-check form-check-inline">
+            <input
+              :checked="type == 'text/plain'"
+              v-model="type"
+              class="form-check-input"
+              type="radio"
+              id="text"
+              value="text/plain"
+              :disabled="sending"
+            />
+            <label class="form-check-label" for="text">Plain Text</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input
+              :checked="type == 'text/html'"
+              v-model="type"
+              class="form-check-input"
+              type="radio"
+              id="html"
+              value="text/html"
+              :disabled="sending"
+            />
+            <label class="form-check-label" for="html">HTML</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input
+              :checked="type == 'text/markdown'"
+              v-model="type"
+              class="form-check-input"
+              type="radio"
+              id="markdown"
+              value="text/markdown"
+              :disabled="sending"
+            />
+            <label class="form-check-label" for="markdown">Markdown</label>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="form-group">
+          <label class="bold">Recipients Settings:</label>
+          <br />
+          <div class="form-check form-check-inline">
+            <input
+              type="radio"
+              v-model="recipients_settings"
+              class="form-check-input"
+              id="separate"
+              value="1"
+              :disabled="sending"
+            />
+            <label class="form-check-label" for="separate">Separate</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input
+              type="radio"
+              v-model="recipients_settings"
+              class="form-check-input"
+              id="included"
+              value="0"
+              :disabled="sending"
+            />
+            <label class="form-check-label" for="included">Included</label>
+          </div>
+        </div>
+        <small id="recipientsHelp" class="form-text text-muted">
+          (Separate) sends the message to each recipient separatly.
+          <br />(Included) includes all recipients in one message.
+        </small>
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="content" class="bold">Message Content:</label>
+      <textarea
+        placeholder="Email Body"
+        :disabled="sending"
+        v-model="body"
+        class="form-control"
+        cols="30"
+        rows="10"
+        v-if="type=='text/plain'"
+      ></textarea>
+      <vue-editor v-model="body" placeholder="Email body" v-if="type=='text/html'"></vue-editor>
+      <div class="row" v-if="type=='text/markdown'">
+        <div class="col">
+          <div class="form-group">
+            <textarea class="form-control" v-model="body" cols="30" rows="10"></textarea>
+          </div>
+        </div>
+        <div class="col">
+          <label class="bold">Message Preview:</label>
+          <markdown-it-vue class="md-body" :content="body" />
         </div>
       </div>
     </div>
     <div class="form-group">
-      <label for="subject">Message Type:</label>
-      <div class="form-check form-check-inline">
-        <input
-          :checked="type == 'text'"
-          v-model="type"
-          class="form-check-input"
-          type="radio"
-          id="text"
-          value="text"
-        />
-        <label class="form-check-label" for="text">Plain Text</label>
-      </div>
-      <div class="form-check form-check-inline">
-        <input
-          :checked="type == 'html'"
-          v-model="type"
-          class="form-check-input"
-          type="radio"
-          id="html"
-          value="html"
-        />
-        <label class="form-check-label" for="html">HTML</label>
-      </div>
-      <div class="form-check form-check-inline">
-        <input
-          :checked="type == 'markdown'"
-          v-model="type"
-          class="form-check-input"
-          type="radio"
-          id="markdown"
-          value="markdown"
-        />
-        <label class="form-check-label" for="markdown">Markdown</label>
-      </div>
-    </div>
-    <div class="form-group">
-      <label for="subject">Message Content:</label>
-      <textarea v-model="body" class="form-control" id cols="30" rows="10"></textarea>
-    </div>
-    <div class="form-group">
-      <button class="btn btn-primary">Send Message</button>
+      <button class="btn btn-primary" @click.prevent="submitForm" :disabled="sending">Send Message</button>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import VueTagsInput from "@johmun/vue-tags-input";
+import { VueEditor } from "vue2-editor";
+import MarkdownItVue from "markdown-it-vue";
+import "markdown-it-vue/dist/markdown-it-vue.css";
+
 export default {
   name: "CreateEmail",
   data() {
     return {
+      sending: false,
       subject: "",
-      type: "text",
+      type: "text/plain",
       body: "",
-      recipients: "",
+      recipients: [],
       tag: "",
+      error: null,
+      success: null,
+      recipients_settings: 1,
       validation: [
         {
           classes: "min-length",
@@ -98,13 +180,55 @@ export default {
       ]
     };
   },
+  methods: {
+    submitForm() {
+      this.sending = true;
+      this.error = null;
+      let addresses = [];
+
+      //Validation
+      if (this.recipients.length < 1) {
+        this.error = "Please add at least 1 recipient";
+        this.sending = false;
+        return false;
+      }
+
+      this.recipients.forEach(recipient => {
+        addresses.push(recipient.text);
+      });
+
+      axios
+        .post("/api/emails/store", {
+          recipients: addresses.toString(),
+          subject: this.subject,
+          body: this.body,
+          recipients_settings: this.recipients_settings,
+          type: this.type
+        })
+        .then(response => {
+          this.success = response.data.message;
+          this.subject = this.body = null;
+          this.type = "text/plain";
+          this.recipients_settings = 1;
+        })
+        .catch(err => {
+          this.error = err;
+        });
+      this.sending = false;
+    }
+  },
   components: {
-    VueTagsInput
+    VueTagsInput,
+    VueEditor,
+    MarkdownItVue
   }
 };
 </script>
 
 <style>
+.bold {
+  font-weight: bold;
+}
 .vue-tags-input {
   background: #324652;
 }
