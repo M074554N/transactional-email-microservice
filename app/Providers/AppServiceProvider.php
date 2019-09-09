665d03bd629861cf055f8ac5c14d7d62da1a5915
Mohamed Hassan <hassan.mohamed.sf@gmail.com>
2019-09-09 09:59:03 +0200

    Update the AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,11 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Arr;
+use Sendgrid;
+use SendGrid\Mail\Mail as SendgridMail;
+use Mailgun\Mailgun;
+use Mailjet\Client as MailjetClient;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +19,36 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton('Mailgun', function ($app) {
+            $config = $app->make('config')->get('services.mailgun', []);
+
+            $apiKey = Arr::pull($config, 'api_key', null);
+
+            return Mailgun::create($apiKey);
+        });
+
+        $this->app->singleton(Sendgrid::class, function ($app) {
+            $config = $app->make('config')->get('services.sendgrid', []);
+
+            $api_key = Arr::pull($config, 'api_key', null);
+
+            return new Sendgrid($api_key);
+        });
+
+        $this->app->bind('SendgridMail', function () {
+            return new SendgridMail();
+        });
+
+        $this->app->singleton('Mailjet', function ($app) {
+            $config = $app->make('config')->get('services.mailjet', []);
+
+            $apiKey = Arr::pull($config, 'api_key', null);
+            $secret = Arr::pull($config, 'secret', null);
+
+            return new MailjetClient($apiKey, $secret);
+        });
+
+        $this->app->tag(['Sendgrid', 'Mailgun', 'Mailjet'], 'providers');
     }
 
     /**
